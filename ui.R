4423065664a33d1a919945ec905611301613aa35
oli2 <oli2@poczta.onet.pl>
2016-10-07 12:00:54 +0100

    second version

--- ui.R
+++ ui.R
@@ -5,13 +5,12 @@ library(shiny)
 
 shinyUI(pageWithSidebar(
   headerPanel("Predicting your childs height"),
-  
   sidebarPanel(
-    numericInput("father", 'Fathers height in inch', 60, min = 50, max = 80, step =.25),
-    numericInput('mother', 'Mothers height in inch', 60, min = 50, max = 80, step =.25),
-    numericInput('children', 'Number of children in your family', 2, min = 1, max = 10, step =1),
-    numericInput('childNum', 'Number of this child within family', 2, min = 1, max = 10, step =1),
-    radioButtons("gender", "Your childs gender",
+    numericInput("father", "Father's height in inch", 60, min = 50, max = 80, step =.25),
+    numericInput('mother', "Mother's height in inch", 60, min = 50, max = 80, step =.25),
+    numericInput('children', "Number of children in your family", 2, min = 1, max = 10, step =1),
+    numericInput('childNum', "Number of this child within family", 2, min = 1, max = 10, step =1),
+    radioButtons("gender", "Your child's gender",
                        c("male" = "male",
                          "female" = "female")),
                       selected = "female",
@@ -22,33 +21,22 @@ shinyUI(pageWithSidebar(
    mainPanel(
      tabsetPanel(
      tabPanel("Prediction", verbatimTextOutput("prediction")),
-     tabPanel("Plot", plotOutput("family_plot"))
+     tabPanel("Plot", plotOutput("family_plot")),
 #      h3("Perdicted height of your child"),
 #      h3(textOutput("prediction"))
+    tabPanel("HowItWorks", 
+             p(h5("This application predicts the height of a child based on following input values:")),
+#            helpText("The calculation is based on the folllowing input data:"),
+             HTML("Father's Height <br>
+                   Mother's Height <br>
+                   Number of children in your family <br>
+                   Number of this child within family <br>
+                   Your child's gender <br>
+             <br> <br>
+            The application uses the predictive model, designed and based on GaltonFamilies data set and has been modeled using Random Forest Machine Learning algorithm
+                      ")
     
       )
-))
+  ))
+ )
 )
-
-# 
-# # Define UI for application that plots random distributions 
-# shinyUI(pageWithSidebar(
-#   
-#   # Application title
-#   headerPanel("Hello Shiny!"),
-#   
-#   # Sidebar with a slider input for number of observations
-#   sidebarPanel(
-#     numericInput('father', 'Numeric input, labeled father', 60, min = 50, max = 80, step = .25),  
-#       selectInput(inputId = "father",
-#                     numericInput('father', 'Glucose mg/dl', 64, min = 60, max = 80, step = .25),
-#                     label = "Father's height in inch",
-#                     choices = c(5.5, 6, 6.5)
-#   ),
-#   
-#   # Show a plot of the generated distribution
-#   mainPanel(
-#     h3("distPlot")
-#   )
-# ))
-# )
