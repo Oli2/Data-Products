0af6abe8f78b0f6ff21fed2b470c81f0f6521d4c
oli2 <oli2@poczta.onet.pl>
2016-10-07 12:12:27 +0100

    third version

--- ui.R
+++ ui.R
@@ -20,23 +20,24 @@ shinyUI(pageWithSidebar(
  #Show the prediciton
    mainPanel(
      tabsetPanel(
-     tabPanel("Prediction", verbatimTextOutput("prediction")),
-     tabPanel("Plot", plotOutput("family_plot")),
-#      h3("Perdicted height of your child"),
-#      h3(textOutput("prediction"))
-    tabPanel("HowItWorks", 
+     tabPanel("HowItWorks", 
              p(h5("This application predicts the height of a child based on following input values:")),
-#            helpText("The calculation is based on the folllowing input data:"),
              HTML("Father's Height <br>
                    Mother's Height <br>
                    Number of children in your family <br>
                    Number of this child within family <br>
                    Your child's gender <br>
-             <br> <br>
+                   <br> <br>
+                  Please click the Submit button once all the fields have been populated.<br>
+                  <br>
             The application uses the predictive model, designed and based on GaltonFamilies data set and has been modeled using Random Forest Machine Learning algorithm
                       ")
     
-      )
+      ),
+     tabPanel("Prediction", verbatimTextOutput("prediction")),
+     tabPanel("Plot", plotOutput("family_plot"))
+     
+     
   ))
  )
 )
